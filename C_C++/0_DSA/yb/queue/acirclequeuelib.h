b10dda466019e884131eb462098e3ea68e6dbd08
Lengocanh <Lengocanh3524@gmail.com>
2024-09-29 22:48:03 +0700

    pqueuelib.h

--- C_C++/0_DSA/yb/queue/acirclequeuelib.h
+++ C_C++/0_DSA/yb/queue/acirclequeuelib.h
@@ -5,49 +5,66 @@
 
 typedef int ElementType;
 
-typedef struct queue{
+typedef struct queue
+{
     ElementType elements[MAX_LENGTH];
     int front, rear;
-}Queue;
+} Queue;
 
-void makeNull(Queue *Q){
+void makeNull(Queue *Q)
+{
     Q->front = 0;
     Q->rear == Q->front;
 }
 
-int isEmpty(Queue Q){
+int isEmpty(Queue Q)
+{
     return Q.front == Q.rear;
 }
 
-int len (Queue Q){
-    return (Q.rear + MAX_LENGTH - Q.front)%MAX_LENGTH;
+int len(Queue Q)
+{
+    return (Q.rear + MAX_LENGTH - Q.front) % MAX_LENGTH;
 }
 
-int isFull(Queue Q){
-    return (Q.rear + 1)% MAX_LENGTH == Q.front;
+int isFull(Queue Q)
+{
+    return (Q.rear + 1) % MAX_LENGTH == Q.front;
 }
 
-void enQueue(ElementType x,Queue *Q){
-    if(!isFull(*Q)){
-        Q->elements[Q->rear] =x;
-        Q->rear = (Q->rear + 1)%MAX_LENGTH;
-    } else {
+void enQueue(ElementType x, Queue *Q)
+{
+    if (!isFull(*Q))
+    {
+        Q->elements[Q->rear] = x;
+        Q->rear = (Q->rear + 1) % MAX_LENGTH;
+    }
+    else
+    {
         printf("<!> Queue is full.");
     }
 }
 
-void deQueue(Queue *Q){
-    if(!isEmpty(*Q)){
-        Q->front = (Q->front + 1)%MAX_LENGTH;
-    } else {
+void deQueue(Queue *Q)
+{
+    if (!isEmpty(*Q))
+    {
+        Q->front = (Q->front + 1) % MAX_LENGTH;
+    }
+    else
+    {
         printf("<!> Queue is empty.");
     }
 }
 
-ElementType front(Queue Q){
-    if(!isEmpty(Q)){
+ElementType front(Queue Q)
+{
+    if (!isEmpty(Q))
+    {
         return Q.elements[Q.front];
-    } else{
+    }
+    else
+    {
         return -1;
     }
 }
